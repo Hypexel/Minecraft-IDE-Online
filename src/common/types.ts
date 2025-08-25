98217082e645eef09ec9d75fa04529c4ce17fa5e
DarkStellar <jasbinderk235@gmail.com>
2025-08-25 15:33:23 +0530

    Update types.ts

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,1 +1,14 @@
+export type FileNode = FileDir | FileFile;
 
+export interface FileDir {
+  type: "dir";
+  name: string;
+  path: string; // relative path from project root, '' for root
+  children?: FileNode[];
+}
+
+export interface FileFile {
+  type: "file";
+  name: string;
+  path: string; // relative path from project root
+}
